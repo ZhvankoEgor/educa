9d11e77bcc103b71eee9e587a0c3be9a8a28cd1d
Zhvanko_Egor <maxmumxxxxxx@gmail.com>
2022-05-17 12:06:44 +0300

    1. Create registration students 2. Add content on modules 3. add enroll on course for students 4. Create course list and detail for students

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.template.loader import render_to_string
+
 from .fields import OrderField
 
 
@@ -27,6 +29,9 @@ class Course(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    students = models.ManyToManyField(User,
+                                      related_name='courses_joined',
+                                      blank=True)
 
     class Meta:
         ordering = ['-created']
@@ -48,6 +53,7 @@ class Module(models.Model):
 
     def __str__(self):
         return f'{self.order}. {self.title}'
+
 
 class Content(models.Model):
     module = models.ForeignKey(Module,
@@ -80,6 +86,11 @@ class ItemBase(models.Model):
 
     def __str__(self):
         return self.title
+
+    def render(self):
+        return render_to_string(
+            f'courses/content/{self._meta.model_name}.html',
+            {'item': self})
 
 
 class Text(ItemBase):
